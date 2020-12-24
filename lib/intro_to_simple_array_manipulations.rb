403256b32c0f6074707fd331771b77f721b5be30
Joshua Morales <joshuapmorales@gmail.com>
2020-12-24 03:35:57 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,39 @@
+def using_concat(arr1, arr2)
+  
+  arr1 = arr1.concat(arr2)
+  
+end
+
+
+def using_insert(arr, element)
+  
+  arr = arr.insert(4, element)  
+  
+end
+
+def using_uniq(arr)
+  
+  arr.uniq
+
+end
+
+
+def using_flatten(arr)
+  
+  arr.flatten
+  
+end
+
+
+def using_delete(arr, string)
+  
+  arr.delete(string)
+  
+end
+
+
+def using_delete_at(arr, int)
+  
+  
+  
+end
